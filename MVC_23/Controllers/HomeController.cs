2e8fc88b5a443c9bdadb28072b25c13da4d2394d
büsra başaran <busrab92@hotmail.com>
2019-05-19 15:59:51 +0300

    .

---
+++ MVC_23/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.SayfaninBasligi = "Bu benim sayfamın başlığı";
+
+            string rol = "Kargocu";
+
+            if (rol=="Kargocu")
+            {
+                ViewBag.AspLinkVisibility = "Hidden";
+            }
+            else
+            {
+                ViewBag.AspLinkVisibility = "Visible";
+            }
+
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC_23/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_23.MvcApplication" Language="C#" %>
